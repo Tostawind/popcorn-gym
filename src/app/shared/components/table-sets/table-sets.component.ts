6c512cca4648bc9f8fa7f1afb9df6b3bc1b99f41
Abel D <adq21791@gmail.com>
2024-03-30 12:59:30 +0100

    feat: set checked style

--- src/app/shared/components/table-sets/table-sets.component.ts
+++ src/app/shared/components/table-sets/table-sets.component.ts
@@ -1,33 +1,36 @@
 import { Component, Input, inject } from '@angular/core';
 import Exercice from '@core/models/exercice.interface';
 import { CountdownService } from '@core/services/countdown.service';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-table-sets',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, CommonModule],
   templateUrl: './table-sets.component.html',
   styleUrl: './table-sets.component.scss'
 })
 export class TableSetsComponent {
   @Input() exercice!: Exercice;
   countdownService = inject(CountdownService);
-  sets: number[] = [];
+  sets: boolean[] = [];
 
   ngOnInit(): void {
     if (this.exercice)  {
-      this.sets = new Array(this.exercice.sets).fill(0);
+      // this.sets = new Array(this.exercice.sets).fill(0);
+      this.sets = new Array(this.exercice.sets).fill(false);
     }
   }
 
 
-  onChecked(event: Event) {
-    // const { checked } = event.target as HTMLInputElement;
-    // if (checked) {
-    //   this.countdownService.startCountdown(exercice.rest);
-    // } else {
-    //   this.countdownService.stopCountdown();
-    // }
+  onChecked(event: Event, index: number) {
+    const { checked } = event.target as HTMLInputElement;
+    if (checked) {
+      this.sets[index] = true;
+      // this.countdownService.startCountdown(exercice.rest);
+    } else {
+      this.sets[index] = false;
+      // this.countdownService.stopCountdown();
+    }
   }
 }
